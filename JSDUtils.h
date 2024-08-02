63ecd8566fe5f5df57496cbcc08e6c1413c57267
Simon <151746540+SDM30@users.noreply.github.com>
2024-08-02 16:31:42 -0500

    Add files via upload

---
+++ JSDUtils.h
@@ -0,0 +1,9 @@
+#ifndef __JSDUtils__H__
+#define __JSDUtils__H__
+
+bool esEntero(char *parametro);
+bool esRacional(char *parametro);
+bool esNumero(char *parametro);
+
+#endif // funciones que se van a
+       // usar en varios componentes
