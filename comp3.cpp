310e41adc1a013b1e6b5288d09a424c892428637
Simon <151746540+SDM30@users.noreply.github.com>
2024-08-02 16:29:08 -0500

    Update comp3.cpp

--- comp3.cpp
+++ comp3.cpp
@@ -2,43 +2,54 @@
 #include <iostream>
 using namespace std;
 
+#include "JSDUtils.h"
 #include "comp3.h"
 
 void ruta_corta(char *i1, char *i2, char *nombre_objeto) {
-  
-  cout << "comando r_cercana" << endl;
-  if(i1 == nullptr || i2)
-  
-  if (nombre_objeto == nullptr)
-    
-    cout << "El nombre del objeto no ha sido cargado" << endl;
-    
-  else if (strcmp(i1, i2) == 0) {
-    
-    cout << "Los indices son iguales" << endl;
-    
-  } else {
-    
-    cout << "Parametro:" << i1 << endl;
-    cout << "Parametro:" << i2 << endl;
-    cout << "Parametro:" << nombre_objeto << endl;
-    
+
+  if (!esNumero(i1) || !esNumero(i2)) {
+
+    cout << "Error: Los parametros deben ser un numero" << endl;
+    return;
   }
-  
+
+  cout << "comando r_cercana" << endl;
+  if (i1 == nullptr || i2)
+
+    if (nombre_objeto == nullptr)
+
+      cout << "El nombre del objeto no ha sido cargado" << endl;
+
+    else if (strcmp(i1, i2) == 0) {
+
+      cout << "Los indices son iguales" << endl;
+      cout << "Parametro:" << i1 << endl;
+      cout << "Parametro:" << i2 << endl;
+
+    } else {
+
+      cout << "Parametro:" << i1 << endl;
+      cout << "Parametro:" << i2 << endl;
+      cout << "Parametro:" << nombre_objeto << endl;
+    }
 }
 
 void ruta_corta_centro(char *i1, char *nombre_objeto) {
-  
+
+  if (!esEntero(i1) || !esRacional(i1)) {
+
+    cout << "Error: Los parametros deben ser un numero" << endl;
+    return;
+  }
+
   cout << "comando r_cercana_centro" << endl;
   if (nombre_objeto == nullptr)
-    
+
     cout << "El nombre del objeto no ha sido cargado" << endl;
-    
+
   else {
-    
+
     cout << "Parametro:" << i1 << endl;
     cout << "Parametro:" << nombre_objeto << endl;
-    
   }
-  
 }
