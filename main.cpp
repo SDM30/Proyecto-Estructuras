27d83483dd4c7454cc0a2cb28b221a1e076f3d01
Simon <151746540+SDM30@users.noreply.github.com>
2024-08-02 12:17:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #include "comp3.h"
 
 void interfazConsola();
+int contarParametros(char *comando);
 
 int main() { interfazConsola(); }
 
@@ -18,70 +19,186 @@ void interfazConsola() {
 
     cout<<"$";
     cin.getline(palabra, 100);
+    int numParametros = contarParametros(palabra);
     char *comando = strtok(palabra," ");
+    
 
     if (strcmp(comando, "cargar") == 0) {
 
-      char *nombre_archivo = strtok(nullptr," ");
-      cargar(nombre_archivo);
+      if(numParametros > 1){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 1);
+        break;
+        
+      }else{
+        
+        char *nombre_archivo = strtok(nullptr," ");
+        cargar(nombre_archivo);
+        cout<<numParametros<<endl;
+        
+      }
 
     } else if (strcmp(comando, "listado") == 0) {
 
-      listado();
+      if(numParametros > 0){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 0);
+        break;
+        
+      }else{
+
+        listado();
+        
+      }
 
     } else if (strcmp(comando, "envolvente") == 0) {
 
-      char *nombre_objeto = strtok(nullptr," ");
-      envolvente(nombre_objeto);
+      if(numParametros > 1){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 1);
+        break;
+        
+      }else{
+
+        char *nombre_objeto = strtok(nullptr," ");
+        envolvente(nombre_objeto);
+        
+      }
 
     } else if (strcmp(comando, "descargar") == 0) {
 
-      char *nombre_objeto = strtok(nullptr," ");
-      descargar(nombre_objeto);
+      if(numParametros > 1){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 1);
+        break;
+        
+      }else{
+
+        char *nombre_objeto = strtok(nullptr," ");
+        descargar(nombre_objeto);
+        
+      }
 
     } else if (strcmp(comando, "guardar") == 0) {
 
-      char *nombre_objeto = strtok(nullptr," ");
-      char *nombre_archivo = strtok(nullptr," ");
-      guardar(nombre_objeto, nombre_archivo);
+      if(numParametros > 2){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 2);
+        break;
+        
+      }else{
+
+        char *nombre_objeto = strtok(nullptr," ");
+        char *nombre_archivo = strtok(nullptr," ");
+        guardar(nombre_objeto, nombre_archivo);
+        
+      }
 
     } else if (strcmp(comando, "salir") == 0) {
 
     } else if (strcmp(comando, "v_cercano") == 0) {
 
-      char *px = strtok(nullptr," ");
-      char *py = strtok(nullptr," ");
-      char *pz = strtok(nullptr," ");
-      char *nombre_objeto = strtok(nullptr," ");
-      v_cercano(px, py, pz, nombre_objeto);
+      if(numParametros > 4){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 4);
+        break;
+        
+      }else{
+
+        char *px = strtok(nullptr," ");
+        char *py = strtok(nullptr," ");
+        char *pz = strtok(nullptr," ");
+        char *nombre_objeto = strtok(nullptr," ");
+        v_cercano(px, py, pz, nombre_objeto);
+        
+      }
 
     } else if (strcmp(comando, "v_cercanos_caja") == 0) {
 
-      char *nombre_objeto = strtok(nullptr," ");
-      v_cercanos_caja(nombre_objeto);
+      if(numParametros > 1){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 1);
+        break;
+        
+      }else{
+
+        char *nombre_objeto = strtok(nullptr," ");
+        v_cercanos_caja(nombre_objeto);
+        
+      }
+
+
 
     } else if (strcmp(comando, "ruta_corta") == 0) {
 
-      char *i1 = strtok(nullptr," ");
-      char *i2 = strtok(nullptr," ");
-      char *nombre_objeto = strtok(nullptr," ");
-      ruta_corta(i1, i2, nombre_objeto);
+      if(numParametros > 3){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 3);
+        break;
+        
+      }else{
+
+        char *i1 = strtok(nullptr," ");
+        char *i2 = strtok(nullptr," ");
+        char *nombre_objeto = strtok(nullptr," ");
+        ruta_corta(i1, i2, nombre_objeto);
+        
+      }
 
     } else if (strcmp(comando, "ruta_corta_centro") == 0) {
 
-       char *i1 = strtok(nullptr," ");
-       char *nombre_objeto = strtok(nullptr," ");
-       ruta_corta_centro(i1, nombre_objeto);
+      if(numParametros > 2){
+        
+        printf("El comando %s solo acepta %d parametros\n",comando, 2);
+        break;
+        
+      }else{
+
+         char *i1 = strtok(nullptr," ");
+         char *nombre_objeto = strtok(nullptr," ");
+         ruta_corta_centro(i1, nombre_objeto);
+        
+      }
 
     } else if (strcmp(comando, "ayuda") == 0) {
 
-      char *param = strtok(nullptr," ");
-      ayuda(param);
+      if(numParametros > 2){
 
+        printf("El comando %s solo acepta %d parametros\n",comando, 2);
+        break;
+
+      }else{
+      
+        char *param = strtok(nullptr," ");
+        ayuda(param);
+        
+      }
+        
     } else {
-      printf(
-          "El comando %s no existe, para ver la lista de comandos use ayuda\n",
-          comando);
+      
+      printf("El comando %s no existe, para ver la lista de comandos use ayuda\n",comando);
+      
     }
+    
   }
+  
+}
+
+int contarParametros(char *comando){
+  
+  int contador = 0;
+  char comandocpy[100];
+  strcpy(comandocpy, comando);
+
+  char *token = strtok(comandocpy, " ");
+
+  while (token != nullptr){
+    
+    token = strtok(nullptr, " ");
+    contador++;
+    
+  }
+
+  return contador - 1;
+  
 }
