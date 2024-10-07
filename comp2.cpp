943c896b296f1c8ba9579ab49b02d8f2e4b5685c
SDM30 <simondiaz@javeriana.edu.co>
2024-10-06 20:56:02 -0500

    quitar std

--- comp2.cpp
+++ comp2.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-using namespace std;
-
 #include "JSDUtils.h"
 #include "comp2.h"
 #include "Sistema3D.h"
@@ -8,43 +6,47 @@ using namespace std;
 
 void v_cercano(char *px, char *py, char *pz, char *nombre_objeto) {
 
-  cout << "comando v_cercano" << endl;
+  std::cout << "comando v_cercano" << std::endl;
 
   if ( !esNumero(px) || !esNumero(py) || !esNumero(pz) ) {
 
-    cout << "Error: Los parametros deben ser un numero" << endl;
+    std::cout << "Error: Los parametros deben ser un numero" << std::endl;
     return;
     
   }
 
   if (px == nullptr || py == nullptr || pz == nullptr) {
 
-    cout << "Error: el comando esperaba las coordenadas del punto completas"
-         << endl;
+    std::cout << "Error: el comando esperaba las coordenadas del punto completas"
+         << std::endl;
     return;
     
   }
 
   Sistema3D sysFiguras;
-  Vertice punto(0,stod(px),stod(py), stod(pz));
+  Vertice punto(0,std::stod(px),std::stod(py), std::stod(pz));
 
   if (nombre_objeto == nullptr) {
 
-    cout << "comando v_cercano sin param objeto" << endl;
-    cout << "Parametro:" << px << endl;
-    cout << "Parametro:" << py << endl;
-    cout << "Parametro:" << pz << endl;
+    std::cout << "comando v_cercano sin param objeto" << std::endl;
+    std::cout << "Parametro:" << px << std::endl;
+    std::cout << "Parametro:" << py << std::endl;
+    std::cout << "Parametro:" << pz << std::endl;
+
+    if (sysFiguras.figs3D.empty()){
+      
+    }
 
   } else {
 
-    cout << "comando v_cercano con param" << endl;
-    cout << "Parametro:" << px << endl;
-    cout << "Parametro:" << py << endl;
-    cout << "Parametro:" << pz << endl;
-    cout << "Parametro:" << nombre_objeto << endl;
+    std::cout << "comando v_cercano con param" << std::endl;
+    std::cout << "Parametro:" << px << std::endl;
+    std::cout << "Parametro:" << py << std::endl;
+    std::cout << "Parametro:" << pz << std::endl;
+    std::cout << "Parametro:" << nombre_objeto << std::endl;
 
     if (!sysFiguras.buscarMalla(nombre_objeto)){
-      std::cout<<"El objeto "<<nombre_objeto<<"no ha sido cargado en memoria"<<std::endl;
+      std::cout<<"El objeto "<<nombre_objeto<<"no ha sido cargado en memoria"<<std::std::endl;
       return;
     }
 
@@ -73,12 +75,12 @@ void v_cercanos_caja(char *nombre_objeto) {
 
   if (nombre_objeto == nullptr) {
 
-    cout << "Error: el comando esperaba un objeto" << endl;
+    std::cout << "Error: el comando esperaba un objeto" << std::endl;
     return;
     
   }
 
-  cout << "comando v_cercano_caja" << endl;
-  cout << "Parametro:" << nombre_objeto << endl;
+  std::cout << "comando v_cercano_caja" << std::endl;
+  std::cout << "Parametro:" << nombre_objeto << std::endl;
   
 }
